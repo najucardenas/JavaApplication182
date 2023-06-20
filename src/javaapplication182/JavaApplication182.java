b00b8588b4d6df5f2902d2aa6a28354071a393b3
najucardenas <103268071+najucardenas@users.noreply.github.com>
2023-06-19 22:03:55 -0500

    primera carga

---
+++ src/javaapplication182/JavaApplication182.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication182;
+
+/**
+ *
+ * @author juan
+ */
+public class JavaApplication182 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       /*ConTem temperatura=new ConTem();
+        temperatura.comTem1();
+        AreaRec rec=new AreaRec();
+        rec.arearc();*/
+       AreaCir cir=new AreaCir();
+       cir.arearc();
+       
+    }
+    
+}
